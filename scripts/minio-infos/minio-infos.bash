653b852c432746ff055356c6e0ef185eec1e30ae
surfict <allexandre@itis.swiss>
2021-04-27 14:58:08 +0000

    Minio infos for all buckets

--- scripts/minio-infos/minio-infos.bash
+++ scripts/minio-infos/minio-infos.bash
@@ -2,7 +2,7 @@
 #
 # Give current usage about minio buckets in GB
 #
-# TODO : Have infos for all buckets
+#
 # http://redsymbol.net/articles/unofficial-bash-strict-mode/
 set -o errexit
 set -o nounset
@@ -10,18 +10,28 @@ set -o pipefail
 IFS=$'\n\t'
 source ../../repo.config
 
+# List of buckets
 
-echo "Infos for Simcore bucket :  ${S3_BUCKET}"
-docker run \
+list_bucket=$(docker run \
 -v /etc/ssl/certs:/etc/ssl/certs:ro \
 --network host \
 --env MC_HOST_myminio="https://${S3_ACCESS_KEY}:${S3_SECRET_KEY}@${S3_ENDPOINT}" \
-minio/mc ls -r --json myminio/${S3_BUCKET} | awk '{ FS=","; print $4 }' | awk '{ FS=":"; n+=$2 } END{ print n }' | xargs echo "0.000000001*" | bc | awk '{print $1" GB"}'
+minio/mc ls myminio)
 
+buckets=$(echo "$list_bucket" | awk '{ FS=" "; print $5 }')
 
-echo "Infos for registry bucket :  ${REGISTRY_S3_BUCKET}"
-docker run \
--v /etc/ssl/certs:/etc/ssl/certs:ro \
---network host \
---env MC_HOST_myminio="https://${S3_ACCESS_KEY}:${S3_SECRET_KEY}@${S3_ENDPOINT}" \
-minio/mc ls -r --json myminio/${REGISTRY_S3_BUCKET} | awk '{ FS=","; print $4 }' | awk '{ FS=":"; n+=$2 } END{ print n }' | xargs echo "0.000000001*" | bc | awk '{print $1" GB"}'
+# Thanks to https://stackoverflow.com/questions/24628076/convert-multiline-string-to-array
+SAVEIFS=$IFS   # Save current IFS
+IFS=$'\n'      # Change IFS to new line
+buckets=($buckets) # split to array $names
+IFS=$SAVEIFS   # Restore IFS
+
+for (( i=0; i<${#buckets[@]}; i++ ))
+do
+    echo "Infos for bucket ${buckets[$i]} : "
+    docker run \
+    -v /etc/ssl/certs:/etc/ssl/certs:ro \
+    --network host \
+    --env MC_HOST_myminio="https://${S3_ACCESS_KEY}:${S3_SECRET_KEY}@${S3_ENDPOINT}" \
+    minio/mc ls -r --json myminio/${buckets[$i]} | awk '{ FS=","; print $4 }' | awk '{ FS=":"; n+=$2 } END{ print n }' | xargs echo "0.000000001*" | bc | awk '{print $1" GB"}'
+done
