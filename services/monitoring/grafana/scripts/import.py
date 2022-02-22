11fcf8236e9d4a62a83f3e3fe448d87f529eb3bb
Dustin Kaiser <kaiser@itis.swiss>
2022-02-22 14:23:22 +0100

    Merge branch 'enhanceGrafanaImportExportScripts' into 'osparc-master.speag.com'
    Enhance grafana import export scripts
    
    See merge request oSparc/osparc-ops-environments!7

--- services/monitoring/grafana/scripts/import.py
+++ services/monitoring/grafana/scripts/import.py
@@ -3,25 +3,26 @@ import os
 import glob
 import json
 import requests
-
+import typer
 
 env = Env()
 env.read_env("./../../../../repo.config", recurse=False)
 
 
-if __name__ == '__main__':
 
-
+def main(foldername: str = ""):
     # We first import the datasources
     url = "https://monitoring." + env.str('MACHINE_FQDN') + "/grafana/api/"
     session = requests.Session()
     session.auth = (env.str('SERVICES_USER'), env.str('SERVICES_PASSWORD'))
     hed = {'Content-Type': 'application/json'}
 
-
-    directories = glob.glob("./../" + env.str('MACHINE_FQDN') + "/datasources/*")
+    if foldername == "":
+        directoriesDatasources = glob.glob("./../" + env.str('MACHINE_FQDN') + "/datasources/*")
+    else:
+        directoriesDatasources = glob.glob(foldername + "/datasources/*")
     print("**************** Add datasources *******************")
-    for file in directories:
+    for file in directoriesDatasources:
         with open(file) as jsonFile:
             jsonObject = json.load(jsonFile)
             jsonFile.close()
@@ -36,15 +37,21 @@ if __name__ == '__main__':
         
         r = session.post(url + "datasources", json = jsonObject, headers=hed)
         print("Import of datasource " + jsonObject["name"])
-        print(r)
+        if r.status_code != 200:
+            print("Received non-200 status code upon import: ", str(r.status_code))
+            print("JSON file failed uploading:")
+            print(json.dumps(jsonObject, sort_keys=True, indent=2))
 
 
     # Second, we import the folders structure
     directoriesData = []
-    directories = glob.glob("./../"+ env.str('MACHINE_FQDN') + "/dashboards/*")
+    if foldername == "":
+        directoriesDashboards = glob.glob("./../" + env.str('MACHINE_FQDN') + "/dashboards/*")
+    else:
+        directoriesDashboards = glob.glob(foldername + "/dashboards/*")
     # We can't create folder with their originial Ids, so we store them and simulate how Grafana will create the new Ids (1, 2, 3, etc)
     countIds = 1
-    for directory in directories:
+    for directory in directoriesDashboards:
         for file in glob.glob(directory + "/*"):
             with open(file) as jsonFile:
                 jsonObject = json.load(jsonFile)
@@ -58,11 +65,14 @@ if __name__ == '__main__':
     for directoryData in directoriesData:
         r = session.post(url + "folders", json = directoryData, headers=hed)
         print("Add folder " + directoryData["title"])
-        print(r)
+        if r.status_code != 200:
+            print("Received non-200 status code upon import: ", str(r.status_code))
+            print("JSON file failed uploading:")
+            print(json.dumps(directoryData, sort_keys=True, indent=2))
 
     print("**************** Add dashboards *******************")
     # Finally we import the dashboards
-    for directory in directories:
+    for directory in directoriesDashboards:
         for file in glob.glob(directory + "/*"):
             with open(file) as jsonFile:
                 jsonObject = json.load(jsonFile)
@@ -76,4 +86,16 @@ if __name__ == '__main__':
                 dashboard["folderId"] = folder[0]["newId"]
                 r = session.post(url + "dashboards/db", json = dashboard, headers=hed)
                 print("Add dashboard " + jsonObject["dashboard"]["title"])
-                print(r)
+                if r.status_code != 200:
+                    print("Received non-200 status code upon import: ", str(r.status_code))
+                    print("JSON file failed uploading:")
+                    print(json.dumps(dashboard, sort_keys=True, indent=2))
+
+if __name__ == "__main__":
+    """
+    Imports grafana dashboard from dumped json files via the Grafana API
+
+    If --foldername is used, the data is taken from this location.
+    Otherwise, the default ops-repo folder is assumed.
+    """
+    typer.run(main)
