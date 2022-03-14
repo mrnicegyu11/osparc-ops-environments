42d4c2d46ead95a2483dd4c57b54c8960a429e8a
Dustin Kaiser <mail@dustinkaiser.eu>
2022-03-14 22:36:57 +0100

    Add new deployment agent

--- services/deployment-agent/src/simcore_service_deployment_agent/__init__.py
+++
@@ -1,6 +0,0 @@
-""" Python package for the simcore_service_deployment_agent.
-
-"""
-from .__version__ import __version__
-from .cli import main
-
--- services/deployment-agent/src/simcore_service_deployment_agent/app_state.py
+++
@@ -1,8 +0,0 @@
-from enum import IntEnum
-
-class State(IntEnum):
-    STARTING = 0
-    RUNNING = 1
-    FAILED = 2
-    STOPPED = 3
-    PAUSED = 4
